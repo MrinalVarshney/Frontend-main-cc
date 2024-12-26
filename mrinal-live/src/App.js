ffd63ffd57a9df1c195ab90e11645d4747348287
MrinalVarshney <varshneymrinal2005@gmail.com>
2024-12-26 07:08:31 +0000

    First commit

---
+++ mrinal-live/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ textAlign: 'center' }}>
+        <h1>Welcome to Frontend 1</h1>
+        <p>This is a simple React app for testing purposes.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
