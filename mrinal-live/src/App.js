afeccd55e3486c6228796ac14a6eece86ab68c8e
MrinalVarshney <varshneymrinal2005@gmail.com>
2024-12-26 11:07:44 +0000

    Update to check webhook

--- mrinal-live/src/App.js
+++ mrinal-live/src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to Frontend 1</h1>
+        <h1>Welcome to Mrinal's website</h1>
         <p>This is a simple React app for testing purposes.</p>
       </div>
     </div>
