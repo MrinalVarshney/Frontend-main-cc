c5928372f4d7b48c138e994c894ad68e0e048d14
MrinalVarshney <varshneymrinal2005@gmail.com>
2024-12-26 11:13:51 +0000

    Update to check webhook

--- mrinal-live/src/App.js
+++ mrinal-live/src/App.js
@@ -4,7 +4,7 @@ function App() {
     <div className="App">
       <div style={{ textAlign: 'center' }}>
         <h1>Welcome to Mrinal's website</h1>
-        <p>This is a simple React app for testing purposes.</p>
+        <p>Explore app using /todo route</p>
       </div>
     </div>
   );
