2f2ebeab0fa3eee6c0a3e6dd281cd9bc2da2e41b
MrinalVarshney <varshneymrinal2005@gmail.com>
2024-12-26 12:11:43 +0000

    Update for testing auto deployment

--- mrinal-live/src/App.js
+++ mrinal-live/src/App.js
@@ -4,6 +4,7 @@ function App() {
     <div className="App">
       <div style={{ textAlign: 'center' }}>
         <h1>Welcome to Mrinal's website</h1>
+        <h2>Currently for testing purpose only</h2>
         <p>Explore app using /todo route</p>
       </div>
     </div>
